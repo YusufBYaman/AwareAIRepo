d8c383ce544ff68a204694cb6b3211ece4e72f9c
YusufBYaman <ysufberaymn@gmail.com>
2026-07-22 11:36:05 +0300

    Task 2 modified

--- Task2/task2.py
+++ Task2/task2.py
@@ -4,6 +4,14 @@ img = cv.imread('image.png')
 
 gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-cv.imshow('Gray Image', gray_img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+if img is not None: 
+
+    cv.imshow('Gray Image', gray_img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    cv.imwrite('gray_image.png', gray_img)
+    print("Resim başarıyla kaydedildi.")
+
+else: 
+    print("Resim yüklenemedi.")
