e6db214ffbbd7f1248499024f3a69b461786da78
YusufBYaman <ysufberaymn@gmail.com>
2026-07-22 13:21:09 +0300

    Task 4 completed

---
+++ Task4/task4.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+img = cv.imread('image.png')
+
+if img is not None:
+
+    x1, y1 = 800, 500
+    x2, y2 = 2300, 2000
+
+    color= (255, 0, 0)  
+
+    cv.rectangle(img, (x1, y1), (x2, y2), color, -1)
+
+    cv.imshow('Image with Rectangle', img)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    cv.imwrite('modified_image.png', img)
+
+else:
+    print("Resim yüklenemedi.")
