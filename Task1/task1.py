cb78c7244351a81926f7706422e685d9b8de9a78
YusufBYaman <ysufberaymn@gmail.com>
2026-07-22 11:12:29 +0300

    First Commit: Task 1 completed

---
+++ Task1/task1.py
@@ -0,0 +1,15 @@
+import cv2
+
+resim = cv2.imread("Gemini_Generated_Image.png")
+
+if resim is not None:
+    cv2.imshow("Resim", resim)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    cv2.imwrite("kaydedilen_resim.png", resim)
+    print("Resim başarıyla kaydedildi.")
+
+else:
+    print("Resim yüklenemedi.")
