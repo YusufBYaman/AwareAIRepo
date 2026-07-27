ba28da0e92b84ad7b71e8fcba8241fe41fbf49a1
YusufBYaman <ysufberaymn@gmail.com>
2026-07-27 10:14:04 +0300

    Task 11 completed

---
+++ Task11/task11.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+img = cv.imread('image.jpg')
+img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+flip_horizontal = cv.flip(img_rgb, 1)
+flip_vertical = cv.flip(img_rgb, 0)
+flip_both = cv.flip(img_rgb, -1)
+
+plt.figure(figsize=(15, 10))
+
+plt.subplot(2, 2, 1)
+plt.imshow(img_rgb)
+plt.title('Orijinal Görüntü')
+plt.axis('off')
+
+plt.subplot(2, 2, 2)
+plt.imshow(flip_horizontal)
+plt.title('Yatay Çevirme (flipCode = 1)')
+plt.axis('off')
+
+plt.subplot(2, 2, 3)
+plt.imshow(flip_vertical)
+plt.title('Dikey Çevirme (flipCode = 0)')
+plt.axis('off')
+
+plt.subplot(2, 2, 4)
+plt.imshow(flip_both)
+plt.title('Hem Yatay Hem Dikey (flipCode = -1)')
+plt.axis('off')
+
+plt.tight_layout()
+plt.show()
